536a90c2f735e6613c884118ff54562855f52cca
Omar Dakelbab <91290810+omar-frontend@users.noreply.github.com>
2022-11-20 15:36:19 +0300

    Update Admin.jsx

--- src/pages/Admin/Admin.jsx
+++ src/pages/Admin/Admin.jsx
@@ -8,28 +8,28 @@ const Admin = () => {
             <div className="admin-page-content">
                 <div className="admin-stats-cards">
                     <div className="stats-card">
-                        <h4>Earnings</h4>
-                        <span>350$</span>
+                        <h4>Users</h4>
+                        <span>259</span>
                     </div>
                     <div className="stats-card">
-                        <h4>Earnings</h4>
-                        <span>350$</span>
+                        <h4>Comments</h4>
+                        <span>2000</span>
                     </div>
                     <div className="stats-card">
-                        <h4>Earnings</h4>
-                        <span>350$</span>
+                        <h4>Blogs</h4>
+                        <span>25</span>
                     </div>
                     <div className="stats-card">
-                        <h4>Earnings</h4>
-                        <span>350$</span>
+                        <h4>Likes</h4>
+                        <span>5000</span>
                     </div>
                     <div className="stats-card">
-                        <h4>Earnings</h4>
-                        <span>350$</span>
+                        <h4>Caravans</h4>
+                        <span>60</span>
                     </div>
                     <div className="stats-card">
-                        <h4>Earnings</h4>
-                        <span>350$</span>
+                        <h4>Rented Caravans</h4>
+                        <span>12</span>
                     </div>
                 </div>
             </div>
